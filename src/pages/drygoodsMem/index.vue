f77212e33dd22cd9e7a7a97c7b61bacda2b48048
Adou <doufu_0329@163.com>
2018-08-26 01:35:31 +0800

    mem页面搭建

--- src/pages/drygoodsMem/index.vue
+++ src/pages/drygoodsMem/index.vue
@@ -1,5 +1,162 @@
 <template>
-    <div style="border:1px solid #f00;">mem{{tabItem.id}}</div>
+    <div class="ad-drygoods-mem">
+      <!--start-->
+
+      <!--MEM是什么-->
+      <div class="global-title drygoods-mem-ssmw">
+        <h2 class="title">
+          MEM是什么?<i></i>
+        </h2>
+        <div class="content">
+          <h3>工商管理硕士Master of Engineering Management</h3>
+          <p>
+            工程管理硕士，是2010年新设置的专业硕士学位。其目的是适应我国现代工程事业发展对工程管理人才的迫切需求，完善工程管理人才培养体系，创新工程管理人才培养模式，提高我国工程管理的人才质量。工程管理硕士的培养注重向学生提供对核心管理领域知识的理解能力，针对工程实践而进行的决策、计划、组织、指挥、协调与控制。
+          </p>
+        </div>
+      </div>
+      <!--为什么要报MEM-->
+      <div class="global-title drygoods-mem-wsmybmem">
+          <h2 class="title">
+            为什么要报MEM?<i></i>
+          </h2>
+          <ul>
+            <li>
+              <p>01</p>
+              <h2>学历</h2>
+              <h3>研究生学历和学位双丰收</h3>
+            </li>
+            <li>
+              <p>02</p>
+              <h2>学历</h2>
+              <h3>研究生学历和学位双丰收</h3>
+            </li>
+            <li>
+              <p>03</p>
+              <h2>学历</h2>
+              <h3>研究生学历和学位双丰收</h3>
+            </li>
+            <li>
+              <p>04</p>
+              <h2>学历</h2>
+              <h3>研究生学历和学位双丰收</h3>
+            </li>
+          </ul>
+      </div>
+      <!--为什么越来越多的工程技术类人员选择MEM?-->
+      <div class="global-title drygoods-mem-wsmylyddgcjslryxzmem">
+        <h2 class="title">
+          为什么越来越多的工程技术类人员选择MEM?<i></i>
+        </h2>
+        <ul>
+          <li>
+            <image src="http://www.a-dou.com/mba/image/mem1.png" mode="aspectFit" />
+            <h2>对比MEM，MEM更“专”</h2>
+            <p>MEM的培养更注重于技术专长、社会经验、项目管理和设计开发等，同时适度补充财务和市场方面的知识，以及增长国际交流经验。MEM为广大工程技术骨干人员未来成长为工程项目管理者，提供了一条性价比更高的发展路径。</p>
+          </li>
+          <li>
+             <image src="http://www.a-dou.com/mba/image/mem2.png" mode="aspectFit" />
+             <h2>对比MEM，MEM更“专”</h2>
+            <p>MEM的培养更注重于技术专长、社会经验、项目管理和设计开发等，同时适度补充财务和市场方面的知识，以及增长国际交流经验。MEM为广大工程技术骨干人员未来成长为工程项目管理者，提供了一条性价比更高的发展路径。</p>
+          </li>
+          <li>
+             <image src="http://www.a-dou.com/mba/image/mem3.png" mode="aspectFit" />
+             <h2>对比MEM，MEM更“专”</h2>
+            <p>MEM的培养更注重于技术专长、社会经验、项目管理和设计开发等，同时适度补充财务和市场方面的知识，以及增长国际交流经验。MEM为广大工程技术骨干人员未来成长为工程项目管理者，提供了一条性价比更高的发展路径。</p>
+          </li>
+        </ul>
+      </div>
+            <!--报考条件-->
+      <div class="global-title drygoods-mba-bktj">
+        <h2 class="title">
+          报考条件<i></i>
+        </h2>
+        <ul>
+          <li>
+            <image src="/static/img/mem_bkbj.png" />
+            <h3>专科学历</h3>
+            <p>毕业之日起至2019年9月</p>
+            <p>满足五年工作年限</p>
+          </li>
+          <li>
+            <image src="/static/img/mem_bkbj.png" />
+            <h3>专科学历</h3>
+            <p>毕业之日起至2019年9月</p>
+            <p>满足五年工作年限</p>
+          </li>
+          <li>
+            <image src="/static/img/mem_bkbj.png" />
+            <h3>专科学历</h3>
+            <p>毕业之日起至2019年9月</p>
+            <p>满足五年工作年限</p>
+          </li>
+          <li>
+            <image src="/static/img/mem_bkbj.png" />
+            <h3>专科学历</h3>
+            <p>毕业之日起至2019年9月</p>
+            <p>满足五年工作年限</p>
+          </li>
+        </ul>
+      </div>
+      <!--备考流程-提前面试-->
+      <div class="global-title drygoods-mba-bklctims">
+        <h2 class="title">
+          备考流程-提前面试<i></i>
+        </h2>
+        <div class="content">
+          <image src="http://www.bqeducation.com/public/static/zp/images/tqms_lc.png" mode="aspectFit" />
+        </div>
+      </div>
+      <!--备课流程-笔试环节-->
+      <div class="global-title drygoods-mba-bklcbshj">
+        <h2 class="title">
+          备课流程-笔试环节<i></i>
+        </h2>
+        <image src="/static/img/bklc.png" mode="aspectFit" />
+      </div>
+      <!--MBA考试形式-->
+      <div class="global-title drygoods-mba-mbaksxs">
+        <h2 class="title">
+          MBA考试形式<i></i>
+        </h2>
+        <div class="content">
+          <image src="/static/img/ksxs.png" mode="aspectFit" />
+          <h3>提前面试<span>部分院校项目开设提前面试</span></h3>
+          <ul>
+            <li>
+              <div>面试时间：</div>
+              <div>格院校自主确定面试时间，一般在5-10月组织多伦次面试。</div>
+            </li>
+            <li>
+              <div>面试形式：</div>
+              <div>面试形式主要分两种，第一种是个人面试；第二种是小组面试。</div>
+            </li>
+          </ul>
+          <h3>管理类联考笔试</h3>
+          <ul>
+            <li>
+              <div>面试时间：</div>
+              <div>格院校自主确定面试时间，一般在5-10月组织多伦次面试。</div>
+            </li>
+            <li>
+              <div>面试形式：</div>
+              <div>面试形式主要分两种，第一种是个人面试；第二种是小组面试。</div>
+            </li>
+          </ul>
+          <h3>复试<span>不同学校有不同的考核要求</span></h3>
+          <ul>
+            <li>
+              <div>面试时间：</div>
+              <div>格院校自主确定面试时间，一般在5-10月组织多伦次面试。</div>
+            </li>
+            <li>
+              <div>面试形式：</div>
+              <div>面试形式主要分两种，第一种是个人面试；第二种是小组面试。</div>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <!--end-->
+    </div>
 </template>
 
 <script>
