f68e3fd7bfe6f9b62dc9c8f3d3a9614d74afeeda
Adou <doufu_0329@163.com>
2018-08-27 22:48:32 +0800

    课程

--- src/pages/course/index.vue
+++ src/pages/course/index.vue
@@ -2,7 +2,15 @@
 <template>
     <!--课程-->
     <div class="ad-course">
-        {{msg}}
+        <swiper class="swiper" indicator-dots="true" autoplay="true" interval="5000" duration="1000">
+            <block v-for="(item, index) in movies" :index="index" :key="item.id">
+                <swiper-item>
+                <a href="http://www.baidu.com/">
+                    <image :src="item.url" class="slide-image" mode="aspectFill" />
+                </a>
+                </swiper-item>
+            </block>
+        </swiper>
     </div>
 </template>
 
@@ -11,7 +19,34 @@ export default {
   name: "courseComponent",
   data() {
     return {
-      msg: "课程"
+      msg: "课程",
+      movies: [
+        {
+          id: 1,
+          url:
+            "http://www.bqeducation.com/public/uploads/20180330/7f4d7b2d06bd73d49a05e59bb119b738.jpg"
+        },
+        {
+          id: 2,
+          url:
+            "http://www.bqeducation.com/public/uploads/20180327/24c9cfb6bb4c0e1260370ca2c7ae3ce3.jpg"
+        },
+        {
+          id: 3,
+          url:
+            "http://www.bqeducation.com/public/uploads/20180426/fed7f4060f3f2af6d1807263d490171a.jpg"
+        },
+        {
+          id: 4,
+          url:
+            "http://www.bqeducation.com/public/uploads/20180327/68e6300c0e25773beabbcf5db7e5730d.jpg"
+        },
+        {
+          id: 5,
+          url:
+            "http://www.bqeducation.com/public/uploads/20180327/cf684c9c10bda9a51330df3626ea8206.jpg"
+        }
+      ]
     };
   }
 };
